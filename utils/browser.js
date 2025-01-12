249d21ef24a77a8a31194ee7e1adea25b7e25dbf
zjuwangrui <chinesewangrui@zju.edu.cn>
2025-01-12 19:27:43 +0800

    add utils functions and dependencise

---
+++ utils/browser.js
@@ -0,0 +1,70 @@
+import logger from './logger';
+/*copyToClipboard:创建一个text的元素插入dom
+printToPdf:把html变成一个pdf
+loadUrlQuery:把url变成对象
+dumpUrlQuery:把对象变成url
+showMessage:alert一条message
+*/
+export function copyToClipboard(text) {
+  const input = document.createElement('textarea');
+  input.style.position = 'fixed';
+  input.style.opacity = 0;
+  input.value = text;
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand('copy');
+  document.body.removeChild(input);
+}
+
+export async function printToPdf(options, html) {
+  const { width, height, margin } = options;
+  html = '<style> div.page { width: ' + (width - margin * 2) + 'px; height: ' + (height - margin * 2) + 'px; } </style>' + html;
+  html = '<style> /* page settings */ @page { size: ' + width + 'px ' + height + 'px; margin: ' + margin + 'px; } </style>' + html;
+  html = '<style> /* normalize browsers */ html, body { margin: 0 !important; padding: 0 !important; } </style>' + html;
+
+  const { style } = options;
+  if (style) {
+    html += '\n\n\n<!-- additional style --><style>' + style + '</style>\n\n\n';
+  }
+
+  // const document = unsafeWindow.document;   // seemingly needless
+  const blob = new Blob([html], { type: 'text/html;charset=utf-8' });
+  const blobUrl = URL.createObjectURL(blob);
+  logger.debug('blobUrl:', blobUrl);
+
+  const $iframe = document.createElement('iframe');
+  $iframe.style.display = 'none';
+  $iframe.src = blobUrl;
+  document.body.appendChild($iframe);
+  $iframe.onload = () => {
+    setTimeout(() => {
+      $iframe.focus();
+      $iframe.contentWindow.print();
+    }, 1);
+  };
+}
+
+export function loadUrlQuery(search) {
+  const query = {};
+  search
+    .slice(1)
+    .split('&')
+    .forEach((item) => {
+      const [key, value] = item.split('=');
+      query[key] = value;
+    });
+  return query;
+}
+
+export function dumpUrlQuery(query) {
+  return (
+    '?' +
+    Object.entries(query)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&')
+  );
+}
+
+export function showMessage(message) {
+  alert(message);
+}
