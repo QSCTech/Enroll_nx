e83c010e8401d9936143162a7c044e1d4890eae8
zjuatri <zjuatri@qq.com>
2025-02-14 16:27:52 +0800

    fix:修复了字体颜色显示不正确的bug，修复了同一课程出现多次的bug，美化了代码

--- src/plugins/zdbk/lessonTableMatch.js
+++ src/plugins/zdbk/lessonTableMatch.js
@@ -18,7 +18,6 @@ export async function lessonTableMatch() {
 
     // 匹配时间得到课程代码
     function renew(timeNumber, semester, prefix) {
-        //let time = `${dayMap[prefix]}第${timeNumber}`;
         const xskcChooseArray = [];
         let xskcChooseElement;
         const xskcdm1 = document.getElementsByClassName('list-group-item');
@@ -31,7 +30,10 @@ export async function lessonTableMatch() {
             if (match) {
                 if (semesterCompare.length === 2 || semester === semesterCompare) {
                     xskcChooseElement = xskcdm1[i].getAttribute('data-xkkh').split('-')[3];
-                    xskcChooseArray.push(xskcChooseElement);
+                    if (xskcChooseArray.length === 0 || xskcChooseElement !== xskcChooseArray.at(-1)) {
+                        xskcChooseArray.push(xskcChooseElement);
+                    }
+
                 }
             }
         }
@@ -39,6 +41,7 @@ export async function lessonTableMatch() {
 
         //匹配复制到新模块
         const xskcdm2 = document.getElementsByClassName('outer_xkxx_list');
+        let oldNode;
         if (xskcChooseArray.length) {
             for (let j = 0; j < xskcChooseArray.length; j++) {
                 let xskcChoose = xskcChooseArray[j];
@@ -46,12 +49,11 @@ export async function lessonTableMatch() {
                     let nameCompare2 = xskcdm2[i].getAttribute('data-xskcdm');
                     if (nameCompare2) {
                         if (nameCompare2.includes(xskcChoose)) {
-
                             let getNode = document.querySelector('.outer_xkxx_list[data-xskcdm="' + nameCompare2 + '"]');
                             if (getNode) {
                                 let newContent = getNode.cloneNode(true);
-                                //copyy.innerHTML = "";
                                 copyy.appendChild(newContent);
+
                             }
                             break;
 
@@ -77,24 +79,23 @@ export async function lessonTableMatch() {
         copyy.innerHTML = "";
 
 
-        if (event.target.closest(".outer_left")||event.target.matches('[data-toggle="tab"]')) {
-            console.log('a')
+        if (event.target.closest(".outer_left") || event.target.matches('[data-toggle="tab"]')) {
             const regex = /^\d+_\d+$/; // 定义正则表达式
 
-            let matchingElements = []
+            let matchingElements = [];
 
 
             const checkInterval = setInterval(() => {
                 if (matchingElements.length > 0) {
                     clearInterval(checkInterval); // 停止等待
-                    console.log(matchingElements);
                     for (let element of matchingElements) {
                         // 获取元素的纯文本内容
-                        const text = element.textContent;
+                        //const text = element.textContent;
+                        const text = element.innerHTML;
 
                         // 创建一个新的 <a> 标签
                         const link = document.createElement('a');
-                        link.textContent = text; // 设置链接的文本内容
+                        link.innerHTML = text; // 设置链接的文本内容
                         link.href = "#"; // 设置链接的目标地址
                         link.className = 'link';
 
@@ -121,41 +122,32 @@ export async function lessonTableMatch() {
             if (event.target.matches(`[id^='${prefix}']`)) {
                 let slot = event.target.id;
                 let timeNumber = slot.split('_')[1];
-
-                let getSemester;
-                let tab = document.querySelector('[aria-expanded="true"]');
-                if (tab) {
-                    getSemester = tab.getAttribute('data-xxq');
-                }
-                else {
-                    const tabs = document.querySelectorAll('[data-toggle="tab"]');
-                    tab = Array.from(tabs).find(element => element.innerHTML.includes('课表'));
-                    getSemester = tab.getAttribute('data-xxq');
-                }
-                renew(timeNumber, getSemester, prefix);
+                const semester = matchSemester();
+                renew(timeNumber, semester, prefix);
             }
-            else if (event.target.parentNode.matches(`[id^='${prefix}']`)) {
-                let slot = event.target.parentNode.id;
-                console.log(slot);
+            else if (event.target.parentNode.parentNode.matches(`[id^='${prefix}']`)) {
+                let slot = event.target.parentNode.parentNode.id;
                 let timeNumber = slot.split('_')[1];
-
-                let getSemester;
-                let tab = document.querySelector('[aria-expanded="true"]');
-                if (tab) {
-                    getSemester = tab.getAttribute('data-xxq');
-                }
-                else {
-                    const tabs = document.querySelectorAll('[data-toggle="tab"]');
-                    tab = Array.from(tabs).find(element => element.innerHTML.includes('课表'));
-                    getSemester = tab.getAttribute('data-xxq');
-                }
-                renew(timeNumber, getSemester, prefix);
+                const semester = matchSemester();
+                renew(timeNumber, semester, prefix);
             };
         }
 
     }, true);
 
-
+    function matchSemester() {
+        let getSemester;
+        let tab = document.querySelector('[aria-expanded="true"]');
+        if (tab) {
+            getSemester = tab.getAttribute('data-xxq');
+        }
+        else {
+            const tabs = document.querySelectorAll('[data-toggle="tab"]');
+            tab = Array.from(tabs).find(element => element.innerHTML.includes('课表'));
+            getSemester = tab.getAttribute('data-xxq');
+        }
+        return getSemester
+    }
 }
 
 
